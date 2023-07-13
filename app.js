8932bd3feb0d0ad42cff3213928b4f66e0b6d8b9
HesaM <hesam.titan.666@gmail.com>
2023-07-14 02:36:59 +0330

    If

--- app.js
+++ app.js
@@ -1,28 +1,58 @@
-console.log("Hello World");
+// console.log("Hello World");
 
-//String
+// //String
 
-let firstName = "hesam";
-let lastName = "behnami";
-let fullName = firstName + " " + lastName;
+// let firstName = "hesam";
+// let lastName = "behnami";
+// let fullName = firstName + " " + lastName;
 
-console.log(fullName);
+// console.log(fullName);
+// ________________________________________;
+// //Numbers
+// let num1 = 5;
+// let num2 = 5;
+// let sum = num1 + num2;
+// let sum2 = num1 - num2;
+// let sum3 = num1 * num2;
+// let sum4 = num1 / num2;
 
-//Numbers
-let num1 = 5;
-let num2 = 5;
-let sum = num1 + num2;
-let sum2 = num1 - num2;
-let sum3 = num1 * num2;
-let sum4 = num1 / num2;
+// console.log(sum);
+// console.log(sum2);
+// console.log(sum3);
+// console.log(sum4);
 
-console.log(sum);
-console.log(sum2);
-console.log(sum3);
-console.log(sum4);
+// let res = num1 + num2 + " " + fullName;
+// console.log(res);
+// let res2 = fullName + num1 + num2;
 
-let res = num1 + num2 + " " + fullName;
-console.log(res);
-let res2 = fullName + num1 + num2;
+// console.log(res2);
+// ________________________________________;
+//if
 
-console.log(res2);
+let age = 20;
+
+if (age > 30) {
+  console.log("Old");
+}
+if (age < 30) {
+  console.log("Young");
+}
+
+let validEmail = false;
+let ValidPassword = true;
+
+if (validEmail && ValidPassword) {
+  console.log("Your sing in");
+} else if (validEmail || ValidPassword) {
+  console.log("your email or password incorrect");
+}
+
+let isUser = false;
+let userRole = "admin";
+if (isUser) {
+  console.log("Wellcome User");
+} else if (userRole === "admin") {
+  console.log("Wellcome admin");
+} else {
+  console.log("Wellcome");
+}
