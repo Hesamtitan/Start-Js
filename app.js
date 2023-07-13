b94e1f76b3be93f82abdbb7bbaaa5c9dd3e120cd
HesaM <hesam.titan.666@gmail.com>
2023-07-13 17:46:23 +0330

    string & numbers

--- app.js
+++ app.js
@@ -1,4 +1,28 @@
-let sum = function(){
+console.log("Hello World");
 
-}
+//String
 
+let firstName = "hesam";
+let lastName = "behnami";
+let fullName = firstName + " " + lastName;
+
+console.log(fullName);
+
+//Numbers
+let num1 = 5;
+let num2 = 5;
+let sum = num1 + num2;
+let sum2 = num1 - num2;
+let sum3 = num1 * num2;
+let sum4 = num1 / num2;
+
+console.log(sum);
+console.log(sum2);
+console.log(sum3);
+console.log(sum4);
+
+let res = num1 + num2 + " " + fullName;
+console.log(res);
+let res2 = fullName + num1 + num2;
+
+console.log(res2);
